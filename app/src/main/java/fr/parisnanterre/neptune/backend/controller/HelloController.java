e2c911d8e5d51b4da624abf43c240d2a99eee57b
KVNS7 <ksoares@outlook.fr>
2024-11-22 17:26:24 +0100

    Add API definition at https://localhost:8080/swagger-ui.html

--- app/src/main/java/fr/parisnanterre/neptune/backend/controller/HelloController.java
+++ app/src/main/java/fr/parisnanterre/neptune/backend/controller/HelloController.java
@@ -3,14 +3,21 @@ package fr.parisnanterre.neptune.backend.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.parisnanterre.neptune.backend.model.Hello;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
+@Tag(name = "Hello Controller")
 public class HelloController {
     
-    @GetMapping("/")
+    @GetMapping("/hello")
+    @Operation(
+            summary = "Returns a welcome message",
+            description = "This endpoint returns a simple message saying 'Hello from API'"
+    )
     public String getHello() {
         return Hello.sayHello();
     }
--- app/src/main/java/fr/parisnanterre/neptune/backend/model/Hello.java
+++ app/src/main/java/fr/parisnanterre/neptune/backend/model/Hello.java
@@ -5,6 +5,6 @@ public class Hello {
     
 
     public static String sayHello(){
-        return "Hello Guys";
+        return "Hello !";
     }
 }
